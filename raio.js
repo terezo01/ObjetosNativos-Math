bbb158837f3e38550d87247246d5c41f5bf68d63
terezo01 <thegames0079@gmail.com>
2024-09-09 09:11:51 -0300

    ,JGTFKTYWJ

--- raio.js
+++ raio.js
@@ -1,8 +1,8 @@
 function calcular(){
-    raio = parseFloat(document.getElementById("raio").value)
+    raio = parseFloat(document.getElementById("raio").value);
 
-    area = Math.PI*Math.pow(raio,2)
-    comprimento = 2*Math.PI*raio
+    area = (Math.PI*Math.pow(raio,2)).toFixed(2);
+    comprimento = (2*Math.PI*raio).toFixed(2);
 
-    document.getElementById("saida").innerHTML = `A área dessa circunferência é: ${area.toFixed(2)} <br> O comprimento é: ${comprimento.toFixed(2)}`
+    document.getElementById("saida").innerHTML = `A área dessa circunferência é: ${area} <br> O comprimento é: ${comprimento}`
 }
